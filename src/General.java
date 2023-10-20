d5c6687450d252b391fead0dcd3c2b8e46ba5c63
USER <jobefremov.a.o@gmail.com>
2023-10-20 21:03:46 +0400

    git remote add origin https://github.com/AOefremov/NewRepository.git

--- src/General.java
+++ src/General.java
@@ -10,8 +10,13 @@ public class General {
         int num1 = input.nextInt();
         System.out.println("Input num2: ");
         int num2 = input.nextInt();
-        if(num1 > num2){
-            System.out.printf(" num1 %s " , num1);
+        System.out.println("Input num3: ");
+        int num3 = input.nextInt();
+        System.out.println("Input num4: ");
+        int num4 = input.nextInt();
+
+        if(num1 > num2 && num3 > num4){
+            System.out.printf(" Key in start ");
         }
         else if(num1 < num2){
             System.out.printf(" num2 %s " , num2);
